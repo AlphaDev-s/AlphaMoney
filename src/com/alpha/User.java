93c45a3ad8840baf81ab32fdc06a6ae03f8ce23a
Maxim Stasenkov <maksim.stasenkov@gmail.com>
2018-10-02 23:55:50 +0300

    ADD(User.java):add data field

--- src/com/alpha/User.java
+++ src/com/alpha/User.java
@@ -3,10 +3,49 @@ package com.alpha;
 import java.util.List;
 
 public class User {
+    private double UID;
     private String firstName;
     private String lastName;
-    private String id;
-    private int accountNumber;
+    private String middleName;
+    private List<String> accountList;
+    private List<String> identificationDocumentList;
+    private List<String> residentialAddressList;
+    private String currentAddress;
 
+    public User() {
+        this.UID = Math.random();
+    }
+
+    public double getUID() {
+        return UID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getCurrentAddress() {
+        return currentAddress;
+    }
 
 }
